43b9147060d0321a5e43fb7b04c9347d8c88aac3
White-Oak <lpzhelud@gmail.com>
2016-06-25 02:57:41 +0300

    Implemented parsing yaourt output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate regex;
+
+use std::process::Command;
+use regex::*;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("yaourt")
+        .arg("-Qe")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    let hello = ::std::str::from_utf8(&output.stdout).unwrap();
+
+    let re = Regex::new(r"(?m)^(\S+)/(\S+) (\S+)(?: \((.*)\))?$").unwrap();
+    let mut pkgs = Vec::new();
+    for cap in re.captures_iter(hello) {
+        let from = cap.at(4).unwrap_or("");
+
+        pkgs.push(Package {
+            name: cap.at(2).unwrap_or(""),
+            group: cap.at(1).unwrap_or(""),
+            version: cap.at(3).unwrap_or(""),
+            meta: from.split(' ').collect(),
+        });
+    }
+
+    for pkg in pkgs {
+        println!("{:?}", pkg)
+    }
+}
+
+#[derive(Debug)]
+struct Package<'a> {
+    name: &'a str,
+    group: &'a str,
+    version: &'a str,
+    meta: Vec<&'a str>,
 }
